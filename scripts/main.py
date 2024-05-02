e1619cc9d9dc931b7de8091de6f554ab37b336a9
popcube <40394657+popcube@users.noreply.github.com>
2024-05-02 20:20:05 +0900

    debug

--- scripts/main.py
+++ scripts/main.py
@@ -7,12 +7,13 @@ def main():
   table = dynamodb.Table('yt_main')
   
   last_items_dict = table.query(
-    KeyConditionExpression=Key("account").eq("pj_sekai"),
+    # KeyConditionExpression=Key("account").eq("pj_sekai"),
     ScanIndexForward=True,
     Limit=1,
     ProjectionExpression="view_25, date_25",
     # ExpressionAttributeNames={"#yr": "year"},
     KeyConditionExpression=(
+        Key("account").eq("pj_sekai") &
         Key("view_25.videos[0].date").exists() &
         Key("view_25.videos[0].title").exists()
       ),
