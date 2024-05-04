7c4fbd04307fa9a4beb04cf5c944af40325ce5b0
popcube <40394657+popcube@users.noreply.github.com>
2024-05-04 17:09:15 +0900

    debug

--- scripts/main.py
+++ scripts/main.py
@@ -7,8 +7,8 @@ from make_timeline import make_timeline
 
 def main(local=False):
   if local:
-    agg_df = pd.read_csv("./agg_df.csv", index_col="date", parse_dates=["date"])
-    agg_diff_df = pd.read_csv("./agg_diff_df.csv", index_col="date", parse_dates=["date"])
+    agg_df = pd.read_csv("./agg_df.csv")
+    # agg_diff_df = pd.read_csv("./agg_diff_df.csv", index_col="date", parse_dates=["date"])
   
   else:    
     scanned_data = scan_data()
@@ -26,20 +26,27 @@ def main(local=False):
       for i in scanned_data
     ]
     agg_df = pd.DataFrame(agg_list, columns=["date", "view_25_views", "view_25_likes", "view_25_comments", "date_25_views", "date_25_likes", "date_25_comments"])
-    agg_df["date"] = pd.to_datetime(agg_df["date"])
-    agg_df.set_index("date", inplace=True)
-    agg_diff_df = pd.concat([
-      agg_df["view_25_views"].diff(),
-      agg_df["date_25_views"].diff(),
-    ], axis="columns").dropna(how="any")
-    
-    agg_df.to_csv("./agg_df.csv")
-    agg_diff_df.to_csv("./agg_diff_df.csv")
+  agg_df["date"] = pd.to_datetime(agg_df["date"]) + pd.Timedelta(hours=9)
+  agg_df.set_index("date", inplace=True)
+  agg_diff_df = pd.concat([
+    agg_df["view_25_views"].diff(),
+    agg_df["date_25_views"].diff(),
+  ], axis="columns").dropna(how="any")
+  
+  agg_df.to_csv("./agg_df.csv")
+  agg_diff_df.to_csv("./agg_diff_df.csv")
   
   make_timeline(agg_df.index, agg_df["view_25_views"], figname="view_25_views")
   make_timeline(agg_diff_df.index, agg_diff_df["view_25_views"], figname="view_25_views_diff")
-  make_timeline(agg_df.index, agg_df["date_25_views"], figname="date_25_views")
-  make_timeline(agg_diff_df.index, agg_diff_df["date_25_views"], figname="date_25_views_diff")
+  for hour in set(agg_df.loc["2024-05-04"].index.hour):
+    make_timeline(
+      agg_df.loc[f"2024-05-04 {hour:02}"].index[1:],
+      agg_df.loc[f"2024-05-04 {hour:02}"]["date_25_views"][1:],
+      figname=f"date_25_views_{hour}")
+    make_timeline(
+      agg_diff_df.loc[f"2024-05-04 {hour:02}"].index[1:],
+      agg_diff_df.loc[f"2024-05-04 {hour:02}"]["date_25_views"][1:],
+      figname=f"date_25_views_diff_{hour}")
 
 if __name__ == "__main__":
   if os.environ.get("AWS_ACCESS_KEY_ID"):
