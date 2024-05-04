31b8d2dfeb90acd8ea0a994a80137d6b2885d473
popcube <40394657+popcube@users.noreply.github.com>
2024-05-04 14:42:50 +0900

    scan test

---
+++ scripts/dyn_api.py
@@ -0,0 +1,61 @@
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+from time import sleep
+import sys
+
+def get_last_hourly_update():
+  dynamodb = boto3.resource('dynamodb')
+  table = dynamodb.Table('yt_main')
+  
+  last_items_dict = table.query(
+    KeyConditionExpression=Key("account").eq("pj_sekai"),
+    ScanIndexForward=False,
+    Limit=120,
+    # ExpressionAttributeNames={"#yr": "year"},
+    FilterExpression=(
+        Attr("view_25.videos[0].date").exists() &
+        Attr("view_25.videos[0].title").exists() 
+      ),
+    ReturnConsumedCapacity="TOTAL"
+    )
+  
+  print(last_items_dict["ConsumedCapacity"]["CapacityUnits"])
+  print()
+  print(len(last_items_dict["Items"]))
+  print(last_items_dict["Items"][0]["view_25"])
+  print(last_items_dict["Items"][0]["date_25"])
+  
+def main():
+  dynamodb = boto3.resource('dynamodb')
+  table = dynamodb.Table('yt_main')
+  scan_param = dict(
+    ReturnConsumedCapacity="TOTAL",
+    Limit=20
+  )
+  scanned_raw = []
+  
+  while True:
+    scanned_raw_dict = table.scan(**scan_param)
+    
+    if scanned_raw_dict["ResponseMetadata"]["HTTPStatusCode"] == 200:
+      scanned_raw += scanned_raw_dict["Items"]
+      if (scanned_raw_dict.get("LastEvaluatedKey", False)):
+        print(f"data succcessfully scanned. count: {scanned_raw_dict["Count"]}, paginating...", flush=True)
+      else:        
+        print(f"data succcessfully scanned. count: {scanned_raw_dict["Count"]}, done!", flush=True)
+        break
+    else:
+      print("ERROR at scan")
+      scanned_raw_dict.pop("Items")
+      print(scanned_raw_dict)
+      raise ValueError("ERROR at scan")
+    
+    scan_param["ExclusiveStartKey"] = scanned_raw_dict["LastEvaluatedKey"]
+    sleep(1) # wait 1 sec
+  
+  # print(len(scanned_raw))
+  # print(scanned_raw[-1])
+  # print(scanned_raw[-1]["fetch_time"])
+
+if __name__ == "__main__":
+  main()
--- scripts/main.py
+++ scripts/main.py
@@ -1,68 +1,24 @@
-import boto3
-from boto3.dynamodb.conditions import Key, Attr
-from datetime import datetime
-from time import sleep
-import sys
+from dyn_api import main as scan_data
+import pandas as pd
+from matplotlib import pyplot as plt
 
-def get_last_hourly_update():
-  dynamodb = boto3.resource('dynamodb')
-  table = dynamodb.Table('yt_main')
-  
-  last_items_dict = table.query(
-    KeyConditionExpression=Key("account").eq("pj_sekai"),
-    ScanIndexForward=False,
-    Limit=120,
-    # ExpressionAttributeNames={"#yr": "year"},
-    FilterExpression=(
-        Attr("view_25.videos[0].date").exists() &
-        Attr("view_25.videos[0].title").exists() 
-      ),
-    ReturnConsumedCapacity="TOTAL"
-    )
-  
-  print(last_items_dict["ConsumedCapacity"]["CapacityUnits"])
-  print()
-  print(len(last_items_dict["Items"]))
-  print(last_items_dict["Items"][0]["view_25"])
-  print(last_items_dict["Items"][0]["date_25"])
-  
 def main():
-  dynamodb = boto3.resource('dynamodb')
-  table = dynamodb.Table('yt_main')
-  scan_param = dict(
-    ReturnConsumedCapacity="TOTAL",
-    Limit=20
-  )
-  scanned_raw = []
+  scanned_data = scan_data()
   
-  while True:
-    scanned_raw_dict = table.scan(**scan_param)
-    # print(len(scanned_raw_dict["Items"]))
-    # print(scanned_raw_dict["LastEvaluatedKey"])
-    # print()
-    
-    # scanned_raw_dict.pop("Items")
-    # print(scanned_raw_dict)
-    # sys.exit(1)
-    
-    if scanned_raw_dict["ResponseMetadata"]["HTTPStatusCode"] == 200:
-      scanned_raw += scanned_raw_dict["Items"]
-      if (scanned_raw_dict.get("LastEvaluatedKey", False)):
-        print(f"data succcessfully scanned. count: {scanned_raw_dict["Count"]}, paginating...", flush=True)
-      else:        
-        print(f"data succcessfully scanned. count: {scanned_raw_dict["Count"]}, done!", flush=True)
-        break
-    else:
-      print("ERROR at scan")
-      scanned_raw_dict.pop("Items")
-      print(scanned_raw_dict)
-      raise ValueError("ERROR at scan")
-    scan_param["ExclusiveStartKey"] = scanned_raw_dict["LastEvaluatedKey"]
-    sleep(1) # wait 1 sec
+  agg_list = [
+    [
+      i["fetch_time"],
+      i["view_25"]["views"],
+      i["view_25"]["likes"],
+      i["view_25"]["comments"],
+      i["date_25"]["views"],
+      i["date_25"]["likes"],
+      i["date_25"]["comments"],
+    ]
+    for i in scanned_data
+  ]
+  agg_df = pd.DataFrame(agg_list, columns=["date", "view_25_views", "view_25_likes", "view_25_comments", "date_25_views", "date_25_likes", "date_25_comments"])
   
-  print(len(scanned_raw))
-  print(scanned_raw[-1])
-  print(scanned_raw[-1]["fetch_time"])
-
-if __name__ == "__main__":
-  main()
+  plt.plot(agg_list["date"], agg_list["view_25_views"])
+  plt.plot(agg_list["date"], agg_list["date_25_views"])
+  plt.savefig("./test.png")
