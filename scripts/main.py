67eed7636ce232fb3be5d28981b49388d6e47508
popcube <40394657+popcube@users.noreply.github.com>
2024-05-04 14:52:50 +0900

    debug

--- scripts/main.py
+++ scripts/main.py
@@ -20,8 +20,13 @@ def main():
   agg_df = pd.DataFrame(agg_list, columns=["date", "view_25_views", "view_25_likes", "view_25_comments", "date_25_views", "date_25_likes", "date_25_comments"])
   
   plt.plot(agg_df["date"], agg_df["view_25_views"])
+  plt.savefig("./test_1.png")
+  plt.plot(agg_df["date"], agg_df["view_25_views"].diff().dropna())
+  plt.savefig("./test_2.png")
   plt.plot(agg_df["date"], agg_df["date_25_views"])
-  plt.savefig("./test.png")
+  plt.savefig("./test_3.png")
+  plt.plot(agg_df["date"], agg_df["date_25_views"].diff().dropna())
+  plt.savefig("./test_4.png")
 
 
 if __name__ == "__main__":
